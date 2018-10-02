24934f97fb41df167cfabb58ffdaab0a5595e0d7
matheus217 <42157250+matheus217@users.noreply.github.com>
2018-10-02 15:27:00 -0300

    Add files via upload

---
+++ testes_do_segui_faixa/testes_do_segui_faixa.ino
@@ -0,0 +1,58 @@
+#include "Segui_Faixa.h"
+
+//Mdireito = 86 -> 180 max (frente)
+//Mesquerdo = 85 -> 0 max (frente)
+//sensorEsquerdo -> 1 nao detecta, 0 detecta
+//sensordireito -> 1 nao detecta, 0 detecta
+
+Segui_Faixa motor;
+bool sensorD, sensorE;
+int pinsensorD = 8;
+int pinsensorE = 13;
+char inicial;
+
+void setup() {
+  pinMode(pinsensorD,INPUT);
+  pinMode(pinsensorE,INPUT);
+  motor.setup(10,11);
+  motor.parar();
+  //sensorD = false; 
+  //sensorE = true;
+  
+
+}
+
+void loop() {
+  Serial.begin(9600);
+
+  sensorD = digitalRead(pinsensorD);
+  sensorE = digitalRead(pinsensorE);
+  Serial.println(motor.getfluxo());
+
+  //Serial.println(sensor);
+  //delay(300);
+
+ if(Serial.available())
+ {
+  
+  inicial = Serial.read();
+  Serial.end();
+ }
+
+ Serial.println(motor.getfluxo());
+    
+  if((inicial=='a'))
+  {
+    Serial.println("entrei");
+    motor.correcao(sensorE, sensorD);
+  }
+  else if(inicial=='p')
+  {
+    motor.parar();
+  }
+  
+  delay(1000);
+  
+  }
+  
+  
